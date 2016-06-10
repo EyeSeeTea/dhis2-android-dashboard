1fdc3af9bcbb2d4013a7139aceda4fbd84edade9
Araz Abishov <araz.abishov.gsoc@gmail.com>
2016-06-10 16:49:04 +0200

    Added new menu icons and corresponding lables for drawer

--- app/src/main/java/org/hisp/dhis/android/HomeActivity.java
+++ app/src/main/java/org/hisp/dhis/android/HomeActivity.java
@@ -11,26 +11,37 @@ import org.hisp.dhis.client.sdk.ui.fragments.WrapperFragment;
 public class HomeActivity extends DefaultHomeActivity {
 
     @IdRes
-    private static final int DRAWER_ITEM_PLACEHOLDER_ID = 324342;
+    private static final int DRAWER_DASHBOARDS_ID = 324322;
+
+    @IdRes
+    private static final int DRAWER_INTERPRETATIONS_ID = 2342342;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addMenuItem(DRAWER_ITEM_PLACEHOLDER_ID, R.drawable.ic_add,
-                R.string.drawer_item_placeholder);
+        addMenuItem(DRAWER_DASHBOARDS_ID, R.drawable.ic_dashboards,
+                R.string.drawer_item_dashboards);
+        addMenuItem(DRAWER_INTERPRETATIONS_ID, R.drawable.ic_interpretations,
+                R.string.drawer_item_interpretations);
+
         if (savedInstanceState == null) {
             onNavigationItemSelected(getNavigationView().getMenu()
-                    .findItem(DRAWER_ITEM_PLACEHOLDER_ID));
+                    .findItem(DRAWER_DASHBOARDS_ID));
         }
     }
 
     @Override
     protected boolean onItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case DRAWER_ITEM_PLACEHOLDER_ID: {
-                attachFragment(WrapperFragment.newInstance(
-                        PlaceholderFragment.class, getString(R.string.drawer_item_placeholder)));
+            case DRAWER_DASHBOARDS_ID: {
+                attachFragment(WrapperFragment.newInstance(PlaceholderFragment.class,
+                        getString(R.string.drawer_item_dashboards)));
+                break;
+            }
+            case DRAWER_INTERPRETATIONS_ID: {
+                attachFragment(WrapperFragment.newInstance(PlaceholderFragment.class,
+                        getString(R.string.drawer_item_interpretations)));
                 break;
             }
         }
