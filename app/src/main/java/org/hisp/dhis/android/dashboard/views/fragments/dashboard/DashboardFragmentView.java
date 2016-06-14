2b861642efa2edd0522796e698d948b2b736404f
Laavanye Bahl <laavanye.bahl@gmail.com>
2016-06-15 00:46:37 +0530

    Files copied from legacy, to be modified : Excluded from building in gradle

---
+++ app/src/main/java/org/hisp/dhis/android/dashboard/views/fragments/dashboard/DashboardFragmentView.java
@@ -0,0 +1,9 @@
+package org.hisp.dhis.android.dashboard.views.fragments.dashboard;
+
+import org.hisp.dhis.client.sdk.ui.bindings.views.View;
+
+    //TODO  Write Code for DashboardFragmentView
+
+public interface DashboardFragmentView extends View{
+
+}
---
+++ app/src/main/java/org/hisp/dhis/android/dashboard/views/fragments/dashboard/DashboardViewPagerFragmentView.java
@@ -0,0 +1,9 @@
+package org.hisp.dhis.android.dashboard.views.fragments.dashboard;
+
+import org.hisp.dhis.client.sdk.ui.bindings.views.View;
+
+    //TODO  Write Code for DashboardViewPagerFragmentView
+
+public interface DashboardViewPagerFragmentView extends View{
+
+}
