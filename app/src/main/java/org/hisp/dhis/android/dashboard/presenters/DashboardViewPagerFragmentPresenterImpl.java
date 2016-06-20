b0a7eed617228176acf40a3b1448960046339a08
Laavanye Bahl <laavanye.bahl@gmail.com>
2016-06-20 20:51:38 +0530

    Code Shifting, Add Some TODOs

--- app/src/main/java/org/hisp/dhis/android/dashboard/presenters/DashboardViewPagerFragmentPresenterImpl.java
+++ app/src/main/java/org/hisp/dhis/android/dashboard/presenters/DashboardViewPagerFragmentPresenterImpl.java
@@ -62,6 +62,7 @@ import static org.hisp.dhis.client.sdk.utils.Preconditions.isNull;
 
 ;
 
+// TODO Remove getFakeData() and loadData properly
 public class DashboardViewPagerFragmentPresenterImpl implements DashboardViewPagerFragmentPresenter {
     private static final String TAG = DashboardViewPagerFragmentPresenterImpl.class.getSimpleName();
     private final DashboardInteractor dashboardInteractor;
@@ -186,15 +187,7 @@ public class DashboardViewPagerFragmentPresenterImpl implements DashboardViewPag
                     }
                 }));
          **/
-
-
-        // BaseIdentifiableObject {id=11, uId=nghVC4wtyzi, name=Antenatal Care, displayName=Antenatal Care, created=2013-09-08T21:47:17.960Z, lastUpdated=2016-06-19T14:42:27.046Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}, BaseIdentifiableObject {id=3, uId=iMnYyBfSxmM, name=Delivery, displayName=Delivery, created=2013-09-09T21:14:14.696Z, lastUpdated=2016-06-19T14:42:42.720Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}, BaseIdentifiableObject {id=15, uId=vqh4MBWOTi4, name=Disease Surveillance, displayName=Disease Surveillance, created=2014-04-06T11:46:01.989Z, lastUpdated=2014-04-14T13:04:29.916Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}, BaseIdentifiableObject {id=4, uId=TAMlzYkstb7, name=Immunization, displayName=Immunization, created=2013-09-09T21:13:49.245Z, lastUpdated=2016-06-14T04:33:33.326Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}, BaseIdentifiableObject {id=17, uId=L1BtjXgpUpd, name=Immunization data, displayName=Immunization data, created=2014-11-03T11:13:52.812Z, lastUpdated=2016-04-21T18:20:14.942Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}, BaseIdentifiableObject {id=16, uId=SCtS6Szuubz, name=Info Videos, displayName=Info Videos, created=2016-04-20T15:43:41.790Z, lastUpdated=2016-04-20T19:45:50.735Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}];
-        List<Dashboard> data = new ArrayList<Dashboard>() ;
-        data.add(Dashboard.createDashboard("Antenatal Care"));
-        data.add(Dashboard.createDashboard("Delivery"));
-        data.add(Dashboard.createDashboard("Disease Surveillance"));
-
-        dashboardViewPagerFragmentView.setDashboards(data);
+        dashboardViewPagerFragmentView.setDashboards(getFakeData());
     }
 
     @Override
@@ -225,4 +218,15 @@ public class DashboardViewPagerFragmentPresenterImpl implements DashboardViewPag
         }
     }
 
+    // Temporary hack for creating Dashboards like legacy.
+    // TODO Use DashboardInteractor to create new Dashboard wherever required
+    private List<Dashboard> getFakeData(){
+        // BaseIdentifiableObject {id=11, uId=nghVC4wtyzi, name=Antenatal Care, displayName=Antenatal Care, created=2013-09-08T21:47:17.960Z, lastUpdated=2016-06-19T14:42:27.046Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}, BaseIdentifiableObject {id=3, uId=iMnYyBfSxmM, name=Delivery, displayName=Delivery, created=2013-09-09T21:14:14.696Z, lastUpdated=2016-06-19T14:42:42.720Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}, BaseIdentifiableObject {id=15, uId=vqh4MBWOTi4, name=Disease Surveillance, displayName=Disease Surveillance, created=2014-04-06T11:46:01.989Z, lastUpdated=2014-04-14T13:04:29.916Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}, BaseIdentifiableObject {id=4, uId=TAMlzYkstb7, name=Immunization, displayName=Immunization, created=2013-09-09T21:13:49.245Z, lastUpdated=2016-06-14T04:33:33.326Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}, BaseIdentifiableObject {id=17, uId=L1BtjXgpUpd, name=Immunization data, displayName=Immunization data, created=2014-11-03T11:13:52.812Z, lastUpdated=2016-04-21T18:20:14.942Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}, BaseIdentifiableObject {id=16, uId=SCtS6Szuubz, name=Info Videos, displayName=Info Videos, created=2016-04-20T15:43:41.790Z, lastUpdated=2016-04-20T19:45:50.735Z, access=Access {manage=true, externalize=true, write=true, read=true, update=true, delete=true}}];
+        List<Dashboard> data = new ArrayList<Dashboard>() ;
+        data.add(Dashboard.createDashboard("Antenatal Care"));
+        data.add(Dashboard.createDashboard("Delivery"));
+        data.add(Dashboard.createDashboard("Disease Surveillance"));
+
+        return data;
+    }
 }
