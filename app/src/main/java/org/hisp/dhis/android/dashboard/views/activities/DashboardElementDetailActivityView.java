8ed8a6480cf7e93f8569b3a126a0abeb7f86f6ae
Laavanye Bahl <laavanye.bahl@gmail.com>
2016-07-24 16:13:01 +0530

    Add DashboardElementDetailActivityPresenterImpl

---
+++ app/src/main/java/org/hisp/dhis/android/dashboard/views/activities/DashboardElementDetailActivityView.java
@@ -0,0 +1,19 @@
+package org.hisp.dhis.android.dashboard.views.activities;
+
+import org.hisp.dhis.client.sdk.models.dashboard.DashboardElement;
+import org.hisp.dhis.client.sdk.models.interpretation.InterpretationElement;
+import org.hisp.dhis.client.sdk.ui.bindings.views.View;
+
+    //TODO  Write Code for DashboardEmptyFragmentView
+
+public interface DashboardElementDetailActivityView extends View{
+
+    void handleDashboardElement(DashboardElement dashboardElement);
+
+    void handleInterpretationElement(InterpretationElement interpretationElement);
+
+    void showError(String message);
+
+    void showUnexpectedError(String message);
+
+}
