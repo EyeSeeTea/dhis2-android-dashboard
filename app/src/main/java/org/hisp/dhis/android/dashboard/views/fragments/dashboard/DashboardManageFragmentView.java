91a4d4bd660877f572aa384c9c46c74473dffda7
Laavanye Bahl <laavanye.bahl@gmail.com>
2016-08-04 21:03:29 +0530

    Add set Current Dashboard method in ManageFragment VIew

--- app/src/main/java/org/hisp/dhis/android/dashboard/views/fragments/dashboard/DashboardManageFragmentView.java
+++ app/src/main/java/org/hisp/dhis/android/dashboard/views/fragments/dashboard/DashboardManageFragmentView.java
@@ -1,5 +1,6 @@
 package org.hisp.dhis.android.dashboard.views.fragments.dashboard;
 
+import org.hisp.dhis.client.sdk.models.dashboard.Dashboard;
 import org.hisp.dhis.client.sdk.ui.bindings.views.View;
 
 public interface DashboardManageFragmentView extends View{
@@ -7,4 +8,10 @@ public interface DashboardManageFragmentView extends View{
     void dismissDialogFragment();
 
     void dashboardNameClearFocus();
+
+    void setCurrentDashboard(Dashboard dashboard);
+
+    void showError(String message);
+
+    void showUnexpectedError(String message);
 }
