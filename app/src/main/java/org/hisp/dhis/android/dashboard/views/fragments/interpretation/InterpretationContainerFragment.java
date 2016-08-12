f11bd16dff4338100d9a2bdad7b9987c49080b4e
Laavanye Bahl <laavanye.bahl@gmail.com>
2016-08-12 16:07:34 +0530

    Use presenter and it's methods in Container Fragment

--- app/src/main/java/org/hisp/dhis/android/dashboard/views/fragments/interpretation/InterpretationContainerFragment.java
+++ app/src/main/java/org/hisp/dhis/android/dashboard/views/fragments/interpretation/InterpretationContainerFragment.java
@@ -29,26 +29,119 @@
 package org.hisp.dhis.android.dashboard.views.fragments.interpretation;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.hisp.dhis.android.dashboard.DashboardApp;
 import org.hisp.dhis.android.dashboard.R;
+import org.hisp.dhis.android.dashboard.UserComponent;
+import org.hisp.dhis.android.dashboard.presenters.interpretation.InterpretationContainerFragmentPresenter;
 import org.hisp.dhis.client.sdk.ui.fragments.BaseFragment;
+import org.hisp.dhis.client.sdk.utils.Logger;
 
-/**
+
+import javax.inject.Inject;
+
+import fr.castorflex.android.smoothprogressbar.SmoothProgressBar;
+
+/*
  *         This fragment is used to make decision, whether to show fragment with
- *         interpretation or fragment with message.
+ *         interpretations or fragment with message.
  */
 
-public class InterpretationContainerFragment extends BaseFragment {
+//TODO  Code for checking fetched data to make decision between ViewPager and EmptyFragment
+
+public class InterpretationContainerFragment extends BaseFragment implements InterpretationContainerFragmentView{
+    private static final String TAG = InterpretationContainerFragment.class.getSimpleName();
+
+    @Inject
+    InterpretationContainerFragmentPresenter interpretationContainerFragmentPresenter;
+
+    @Inject
+    Logger logger;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_empty, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        UserComponent userComponent = ((DashboardApp) getActivity().getApplication()).getUserComponent();
+        userComponent.inject(this);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        interpretationContainerFragmentPresenter.attachView(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        logger.d(TAG, "onResume()");
+        interpretationContainerFragmentPresenter.attachView(this);
+        logger.d(TAG, "onLoadLocalData()");
+        interpretationContainerFragmentPresenter.onLoadLocalData();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        logger.d(TAG, "onPause()");
+        interpretationContainerFragmentPresenter.detachView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        logger.d(TAG, "onDestroy()");
+    }
+
+    @Override
+    public boolean onBackPressed() {
+        return true;
+    }
+
+    @Override
+    public void navigationAfterLoadingData(Boolean hasData) {
+        if (hasData) {
+            // we don't want to attach the same fragment
+            if (!isFragmentAttached(InterpretationFragment.TAG)) {
+                attachFragment(new InterpretationFragment(),
+                        InterpretationFragment.TAG);
+            }
+        } else {
+            if (!isFragmentAttached(InterpretationEmptyFragment.TAG)) {
+                attachFragment(new InterpretationEmptyFragment(),
+                        InterpretationEmptyFragment.TAG);
+            }
+        }
+    }
+
+    private void attachFragment(Fragment fragment, String tag) {
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.fragment_content_frame, fragment, tag)
+                .commitAllowingStateLoss();
+    }
+
+    private boolean isFragmentAttached(String tag) {
+        return getChildFragmentManager().findFragmentByTag(tag) != null;
+    }
+
+    @NonNull
+    protected Toolbar getToolbar() {
+        return getParentToolbar();
     }
 
 }
