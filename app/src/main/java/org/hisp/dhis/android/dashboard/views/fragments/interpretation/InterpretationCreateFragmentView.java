74ec3909149e5ae3f31046e2c09a133e33ec4114
Laavanye Bahl <laavanye.bahl@gmail.com>
2016-08-13 21:54:41 +0530

    Add View for InterpretationCreateFragment with required methods

---
+++ app/src/main/java/org/hisp/dhis/android/dashboard/views/fragments/interpretation/InterpretationCreateFragmentView.java
@@ -0,0 +1,18 @@
+package org.hisp.dhis.android.dashboard.views.fragments.interpretation;
+
+import org.hisp.dhis.client.sdk.models.dashboard.DashboardElement;
+import org.hisp.dhis.client.sdk.models.dashboard.DashboardItem;
+import org.hisp.dhis.client.sdk.ui.bindings.views.View;
+
+import java.util.List;
+
+public interface InterpretationCreateFragmentView extends View{
+
+    void showError(String message);
+
+    void showUnexpectedError(String message);
+
+    void setCurrentDashboardItem(DashboardItem dashboardItem);
+
+    void setDashboardElements(List<DashboardElement> dashboardElements);
+}
