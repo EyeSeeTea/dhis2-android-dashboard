5c041edb681e1aa062d0c1ea1bb0ba7581df15f0
ifoche <nacho.foche@gmail.com>
2017-06-21 07:55:35 +0200

    add traces to script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -6,5 +6,5 @@ gitPath=$(git rev-parse --show-toplevel)
 # Generate last commit
 sh ${gitPath}/generate_last_commit.sh
 
+echo "Generate Test Coverage Report:"
 ./gradlew build jacocoTestReport assembleAndroidTest --stacktrace
-./gradlew connectedCheck --stacktrace
